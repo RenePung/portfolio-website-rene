a50978171a8238e142e9bd8601617a3177f6375a
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-10 18:02:09 +0100

    update

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -5,13 +5,14 @@ import { motion } from 'framer-motion';
 
 // section divider ( | )
 export default function SectionDivider() {
-  return (
-    <motion.div 
-    className="bg-gray-300 my-24 h-16 w-1 rounded-full hidden sm:block"
-    initial={{ opacity: 0, y: 100 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ delay: 0.125 }}
-    >
-    </motion.div>
-  )
-}
+    return (
+      <motion.div 
+      className="bg-gray-300 my-24 h-16 w-1 rounded-full hidden sm:block"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.125 }}
+      >
+      </motion.div>
+    )
+  }
+  
