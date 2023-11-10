f2f5fcbcc1221da71b17fb461af5108f1877c4a1
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-10 10:39:35 +0100

    added styling to an image

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,7 +6,15 @@ export default function Intro() {
     <section>
         <div className="flex items-center justify-center">
             <div>
-                <Image src="https://images.unsplash.com/photo-1699116550661-bea051952f96?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="RP" width="192" height="192" quality="95" priority={true} /> {/*image at the start*/}
+                <Image 
+                src="https://images.unsplash.com/photo-1699116550661-bea051952f96?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                alt="RP" 
+                width="192" 
+                height="192" 
+                quality="95" 
+                priority={true}
+                className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl" 
+                /> {/*image at the start*/}
             </div>
         </div>
     </section>
