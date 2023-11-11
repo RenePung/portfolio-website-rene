609715920062f16b89f7a9a61bb979c03751435d
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-11 16:31:43 +0100

    changed style of hovering

--- components/header.tsx
+++ components/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
     ></motion.div>
 
     <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-      <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+      <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-950 sm:w-[initial] sm:flex-nowrap sm:gap-5">
         {links.map((link) => (
             <motion.li 
             className="h-3/4 flex items-center justify-center relative" 
@@ -25,7 +25,7 @@ export default function Header() {
             >
 
             <Link 
-            className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition" 
+            className="flex w-full items-center justify-center px-3 py-3 hover:text-sky-500 transition" 
             href={link.hash}
             >
             {link.name}
