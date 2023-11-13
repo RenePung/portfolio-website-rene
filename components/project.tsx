e642b17862bbd7cf5c3a3457e0445d45b49bef06
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-13 18:29:55 +0100

    added shadow to cards

--- components/project.tsx
+++ components/project.tsx
@@ -31,7 +31,7 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-sky-400 hover:bg-sky-200 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] transition sm:group-even:pl-8">
+      <section className="bg-gray-100 max-w-[42rem] border border-sky-400 hover:bg-sky-200 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] transition sm:group-even:pl-8 shadow-md shadow-sky-300/70 hover:shadow-sky-500/80">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">
