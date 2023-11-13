ac4a42be43fcd1b7109a843cb71f8263ce300c4c
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-13 11:24:38 +0100

    changed bg color and created projects file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/about";
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 //----------------imports----------------------------------
 
@@ -10,5 +11,6 @@ export default function Home() {
     <Intro />
     <SectionDivider />
     <About />
+    <Projects />
   </main>;
 }
