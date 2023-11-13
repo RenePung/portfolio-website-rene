638fa71c548be470420873da97507079404e0394
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-13 08:22:50 +0100

    created projects section and added it to our page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import About from "@/components/about";
 import Intro from "@/components/intro";
+import { Projects } from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 //----------------imports----------------------------------
 
 
-// centering all sections on our page
+// centering all sections on our page - every section file (example: intro.tsx, projects.tsx etc.) made has to be imported in here for a structure of the page!
 export default function Home() {
   return <main className="flex flex-col items-center px-4">
     <Intro />
     <SectionDivider />
     <About />
+    <Projects />
   </main>;
 }
---
+++ components/projects.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+export const Projects = () => {
+  return (
+    <section>
+        <SectionHeading>My Recent Projects</SectionHeading>
+    </section>
+  )
+}
