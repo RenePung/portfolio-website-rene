ef2f24c30eb2e70549b0deafac4ab9d64dc77442
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-13 13:10:51 +0100

    added text, description, h3 and images to our project section

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import SectionHeading from './section-heading'
+import { projectsData } from '@/lib/data'
+import Image from 'next/image';
+//---------------------------------imports---------------------------------------------------------
+
+
+// change data by going to the data.ts file in lib folder and change images by going to the public folder
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <section>
+      <SectionHeading>My Recent Projects</SectionHeading>
+      <div>
+        {
+          projectsData.map((project, index) => (
+            <React.Fragment key={index} >
+            <Project {...project} />
+            </React.Fragment>
+          ))}
+      </div>
+    </section>
   )
+}
+
+
+// new custom component
+type ProjectProps = (typeof projectsData)[number];
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+  return <section>
+
+    <h3>{title}</h3>
+
+    <p>{description}</p>
+
+    <ul>
+      {tags.map((tag, index) => (
+        <li key={index}>{tag}</li>
+      ))}
+    </ul>
+
+    <Image src={imageUrl} alt="Most Recent Project I Worked On" quality={95} />
+  </section>
 }
