d4b5f41fcdf43ccdab5a4de27635add7f264626b
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-13 16:39:34 +0100

    completed projects section

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import SectionHeading from './section-heading'
-import { projectsData } from '@/lib/data'
-import Image from 'next/image';
+import React from 'react';
+import SectionHeading from './section-heading';
+import { projectsData } from '@/lib/data';
+import Project from './project';
+
 //---------------------------------imports---------------------------------------------------------
 
 
-// change data by going to the data.ts file in lib folder and change images by going to the public folder
-
+// change data by going to the data.ts file in lib folder and change images by going to the public folder.
+// to change colors / styling go to projects.tsx file in the same folder as you are in.
 export default function Projects() {
   return (
     <section>
@@ -23,32 +24,3 @@ export default function Projects() {
   )
 }
 
-// new custom component
-type ProjectProps = (typeof projectsData)[number];
-
-function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  return (
-
-    <section className="group bg-gray-100 max-w-[42rem] border border-sky-400 hover:bg-sky-200 transition rounded-lg overflow-hidden last:mb-0 mb-3 sm:mb-8 sm:pr-8 relative sm:h-[20rem] sm:even:pl-8">
-
-    <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-    
-    <h3 className="text-2xl font-semibold">{title}</h3>
-
-    <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-
-    <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-      {tags.map((tag, index) => (
-        <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full" key={index}>{tag}</li>
-      ))}
-    </ul>
-    </div>
-
-    <Image 
-    src={imageUrl} 
-    alt="Most Recent Project I Worked On" 
-    quality={95} 
-    className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 transition  group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3  group-even:group-hover:rotate-2" />
-  </section>
-  );
-}
