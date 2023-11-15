d59e3bd023ec6076e44d494025541905546bf1ba
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-15 14:09:24 +0100

    added missing icon

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SectionHeading from './section-heading'
 import { FaPaperPlane } from 'react-icons/fa'
+import { AiOutlineSend } from "react-icons/ai";
 //-------------------------------------imports---------------------------------------------------
 
 export default function Contact() {
@@ -12,7 +13,7 @@ export default function Contact() {
         <form className="mt-10 flex flex-col">
             <input className="h-14 rounded-lg borderBlack" type="email" />
             <textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
-            <button className="bg-sky-500 hover:bg-sky-700 text-white px-7 py-3 flex items-center gap-2 rounded-full h-[3rem] w-[8rem]" type="submit" >Send <FaPaperPlane /></button>
+            <button className="bg-sky-500 hover:bg-sky-700 text-white px-7 py-3 flex items-center gap-2 rounded-full h-[3rem] w-[8rem]" type="submit" >Send <AiOutlineSend /></button>
         </form>
     </section>
   )
