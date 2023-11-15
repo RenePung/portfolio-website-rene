5a6a3c5ed29485fefe5a67a942612549a68c0ffb
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-15 09:44:40 +0100

    react vertical timeline any type bug fixed

---
+++ components/experience.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionHeading from './section-heading';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+//-------------imports---------------------------------------------------------
+
+export default function Experience() {
+  return (
+    <section id="experience">
+        <SectionHeading>Experience</SectionHeading>
+    </section>
+  )
+}
