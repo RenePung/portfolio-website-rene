8158fbd7de627ecc77df050c589bb07c835c6eb5
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-15 14:38:58 +0100

    added styling of form

--- components/contact.tsx
+++ components/contact.tsx
@@ -8,12 +8,16 @@ export default function Contact() {
   return (
     <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center">
         <SectionHeading>Contact Me</SectionHeading>
-        <p className="text-gray-700">You can contact at <a className="underline decoration-sky-300 hover:decoration-emerald-400 decoration-2" href="mailto:rene.pungartnik98@gmail.com">rene.pungartnik98@gmail.com</a> or trough this form!</p>
+        <p className="text-gray-700">You can contact me at <a className="underline decoration-sky-300 hover:decoration-emerald-400 decoration-2" href="mailto:rene.pungartnik98@gmail.com">rene.pungartnik98@gmail.com</a> or trough this form!</p>
 
         <form className="mt-10 flex flex-col">
-            <input className="h-14 rounded-lg borderBlack" type="email" />
-            <textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
-            <button className="bg-sky-500 hover:bg-sky-700 text-white px-7 py-3 flex items-center gap-2 rounded-full h-[3rem] w-[8rem]" type="submit" >Send <AiOutlineSend /></button>
+            <input className="h-14 px-4 rounded-lg borderBlack" type="email" placeholder="Enter your email" />
+            <textarea className="h-52 my-3 rounded-lg borderBlack p-4" placeholder="Enter message here" />
+            <button 
+            className="group bg-sky-500 hover:bg-sky-700 text-white rounded-full h-[3rem] w-[8rem] outline-none transition-all flex items-center justify-center gap-2 focus:scale-110 hover:scale-110 active:scale-105"
+             type="submit" 
+             >
+                Send <AiOutlineSend className="opacity-90 transition-all group-hover:translate-x-1" /></button>
         </form>
     </section>
   )
