4e7629b4e8e69ce7c0d2b3b8a760dc922618be0f
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-15 16:44:51 +0100

    update form

--- components/contact.tsx
+++ components/contact.tsx
@@ -31,10 +31,13 @@ export default function Contact() {
         <SectionHeading>Contact Me</SectionHeading>
         <p className="text-gray-700 -mt-6">You can contact me at <a className="underline decoration-sky-300 hover:decoration-emerald-400 decoration-2" href="mailto:rene.pungartnik98@gmail.com">rene.pungartnik98@gmail.com</a> or trough this form!</p>
 
-        <form className="mt-10 flex flex-col">
-            <input className="h-14 px-4 rounded-lg borderBlack" type="email" required maxLength={500} placeholder="Enter your email" />
+        <form className="mt-10 flex flex-col" action={async (formData) => {
+         console.log(formData.get("senderEmail"));
+         console.log(formData.get("message"));
+        }}>
+            <input className="h-14 px-4 rounded-lg borderBlack" name="senderEmail" type="email" required maxLength={500} placeholder="Enter your email" />
 
-            <textarea className="h-52 my-3 rounded-lg borderBlack p-4" required maxLength={500} placeholder="Enter message here" />
+            <textarea className="h-52 my-3 rounded-lg borderBlack p-4" name="message" required maxLength={500} placeholder="Enter message here" />
 
             <button 
             className="group bg-sky-500 hover:bg-sky-700 text-white rounded-full h-[3rem] w-[8rem] outline-none transition-all flex items-center justify-center gap-2 focus:scale-110 hover:scale-110 active:scale-105"
