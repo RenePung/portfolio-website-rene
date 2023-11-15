700c54e78c1f68d2fd231f872ccf7288648a683c
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-15 14:06:40 +0100

    started contact me section

---
+++ components/contact.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+import { FaPaperPlane } from 'react-icons/fa'
+//-------------------------------------imports---------------------------------------------------
+
+export default function Contact() {
+  return (
+    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center">
+        <SectionHeading>Contact Me</SectionHeading>
+        <p className="text-gray-700">You can contact at <a className="underline decoration-sky-300 hover:decoration-emerald-400 decoration-2" href="mailto:rene.pungartnik98@gmail.com">rene.pungartnik98@gmail.com</a> or trough this form!</p>
+
+        <form className="mt-10 flex flex-col">
+            <input className="h-14 rounded-lg borderBlack" type="email" />
+            <textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
+            <button className="bg-sky-500 hover:bg-sky-700 text-white px-7 py-3 flex items-center gap-2 rounded-full h-[3rem] w-[8rem]" type="submit" >Send <FaPaperPlane /></button>
+        </form>
+    </section>
+  )
+}
