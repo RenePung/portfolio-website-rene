e121ae8831cade486c714469537dd2d64b44a8c5
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-15 15:15:49 +0100

    added framer motion to our section

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import SectionHeading from './section-heading'
-import { FaPaperPlane } from 'react-icons/fa'
+"use client"; // we use this because of framer-motion
+import React from 'react';
+import SectionHeading from './section-heading';
+import { FaPaperPlane } from 'react-icons/fa';
 import { AiOutlineSend } from "react-icons/ai";
+import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
 //-------------------------------------imports---------------------------------------------------
 
 export default function Contact() {
+    const { ref } = useSectionInView("Contact");
+
   return (
-    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center">
+    <motion.section
+     ref={ref}
+     id="contact"
+     className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+     initial={{
+        opacity: 0,
+     }}
+     whileInView={{
+        opacity: 1,
+     }}
+     transition={{
+        duration: 1,
+     }}
+     viewport={{
+        once: true, // this makes animation only once. if we remove this animation will be everytime section is in the view
+     }}
+     >
         <SectionHeading>Contact Me</SectionHeading>
-        <p className="text-gray-700">You can contact me at <a className="underline decoration-sky-300 hover:decoration-emerald-400 decoration-2" href="mailto:rene.pungartnik98@gmail.com">rene.pungartnik98@gmail.com</a> or trough this form!</p>
+        <p className="text-gray-700 -mt-6">You can contact me at <a className="underline decoration-sky-300 hover:decoration-emerald-400 decoration-2" href="mailto:rene.pungartnik98@gmail.com">rene.pungartnik98@gmail.com</a> or trough this form!</p>
 
         <form className="mt-10 flex flex-col">
             <input className="h-14 px-4 rounded-lg borderBlack" type="email" placeholder="Enter your email" />
@@ -19,6 +40,6 @@ export default function Contact() {
              >
                 Send <AiOutlineSend className="opacity-90 transition-all group-hover:translate-x-1" /></button>
         </form>
-    </section>
+    </motion.section>
   )
 }
