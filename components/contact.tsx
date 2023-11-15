8c4c2f90fc3bb351a22fff266dfe65062e73cd21
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-15 16:13:53 +0100

    edited text area

--- components/contact.tsx
+++ components/contact.tsx
@@ -32,13 +32,16 @@ export default function Contact() {
         <p className="text-gray-700 -mt-6">You can contact me at <a className="underline decoration-sky-300 hover:decoration-emerald-400 decoration-2" href="mailto:rene.pungartnik98@gmail.com">rene.pungartnik98@gmail.com</a> or trough this form!</p>
 
         <form className="mt-10 flex flex-col">
-            <input className="h-14 px-4 rounded-lg borderBlack" type="email" placeholder="Enter your email" />
-            <textarea className="h-52 my-3 rounded-lg borderBlack p-4" placeholder="Enter message here" />
+            <input className="h-14 px-4 rounded-lg borderBlack" type="email" required maxLength={500} placeholder="Enter your email" />
+
+            <textarea className="h-52 my-3 rounded-lg borderBlack p-4" required maxLength={500} placeholder="Enter message here" />
+
             <button 
             className="group bg-sky-500 hover:bg-sky-700 text-white rounded-full h-[3rem] w-[8rem] outline-none transition-all flex items-center justify-center gap-2 focus:scale-110 hover:scale-110 active:scale-105"
              type="submit" 
              >
-                Send <AiOutlineSend className="opacity-90 transition-all group-hover:translate-x-1" /></button>
+                Send <AiOutlineSend className="opacity-90 transition-all group-hover:translate-x-1" />
+            </button>
         </form>
     </motion.section>
   )
